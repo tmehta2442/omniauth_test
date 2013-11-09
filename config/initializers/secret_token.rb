e7af356fd15c229d0be741d0ae05282d9459700f
tmehta2442 <tejas@bootinguptj.com>
2013-11-09 15:37:55 -0800

    first commit ominiauth for Pothole Patrol

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OmniauthTest::Application.config.secret_token = 'd953991a2996005293fee38aba2c5884f168b17ef0846154a6d3e9efeeae32cf3d776f9a8d2bdaad61e1762983ab3839fda1c0a11e458ed29be5dd8bcd883036'
