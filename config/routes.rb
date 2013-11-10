34d488183f201b17a16bcc1c90f60d49afe48e4c
tmehta2442 <tejas@bootinguptj.com>
2013-11-09 16:19:10 -0800

    routes modified for login/callback/failure

--- config/routes.rb
+++ config/routes.rb
@@ -1,58 +1,18 @@
 OmniauthTest::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
+  # get "sessions/new"
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
+  # get "sessions/create"
 
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
+  # get "sessions/failure"
 
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  # get "sessions/destroy"
+# above original routes, not needed
 
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
 
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+#create a simple login form where the user will see "connect w/ FB"
+  get '/login', :to => 'sessons#new', :as => :login
+#catch the callback, once app is authorized, providers redirects to the url, and we can sue the data
+  match '/auth/:provider/callback', :to => 'sessions#create'
+#problem/if user didn't authorize the app  
+  match '/auth/failure', :to => 'session#failure'
 end
