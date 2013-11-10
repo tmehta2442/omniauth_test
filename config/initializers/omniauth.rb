12792b1d7df989abfe98cf428596fa8e808f9d4e
tmehta2442 <tejas@bootinguptj.com>
2013-11-09 23:29:25 -0800

    update to controller

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 # authentication providers will be configured via this file
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, "1429168373963949", "0edfb2f9edd200ca4a205eb51976e18f"
+	provider :facebook, "1429168373963949", "c40b930fac1b2b0eb06ea87d54dccb2f"
 end
